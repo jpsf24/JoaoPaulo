e2316814936db4589aba4415ce4ee0ba366c3ece
jpsf24 <jpsf24@gmail.com>
2020-05-19 12:34:23 +0100

    First commit

---
+++ hello.py
@@ -0,0 +1,5 @@
+def first():
+    print('John')
+
+def last():
+    print('Paul')
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='JoaoPaulo',
+      version='10.0',
+      description='JoaoPaulo Distribution Module',
+      author='Your Name',
+      author_email='Your Email',
+      url='The URL project - github for example',
+      py_modules=['JoaoPaulo'],
+     )
